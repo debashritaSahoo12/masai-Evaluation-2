2b778bfa454dec33c181f7eca8ccedccb44a0d3e
Debashrita Sahoo <138135798+debashritaSahoo12@users.noreply.github.com>
2025-03-23 14:13:10 +0530

    Update Student_Performance.js

--- Student_Performance.js
+++ Student_Performance.js
@@ -43,48 +43,45 @@ function createStudentManager() {
           }
         });
     },
-    generateInsight: function () {
-      let insights = {
-        topPerformers: [],
-        difficultSub: {},
-        failedStudent: [],
-        frequencysub: {},
-      };
-      let studentData=Object.values(students).map((student)=>{
-        let score=Object.values(student.subject);
-        let avg=score.reduce((acc,[_,scores])=>acc+scores,0)/score.length
-        if(avg>85){
-            insights.topPerformers.push(student.name)
+          generateInsights : function (){
+            let insights = {
+                topPerformers : [],
+                difficultSub : {},
+                failedStudents : [],
+                subFrequency : {}
+            };
 
+            let studentData = Object.values(students).map(student =>{
+                let scores = Object.entries(student.subject);
+                let avgScore = scores.length ? scores.reduce((acc, [_, score])=> acc + score, 0) / scores.length : 0;
+
+                if(avgScore > 85) insights.topPerformers.push(student.name);
+                if(scores.some(([_, score])=> score < 35)) insights.failedStudents.push(student.name);
+
+                scores.forEach(([subject])=>{
+                    insights.subFrequency[subject] = (insights.subFrequency[subject] || 0) + 1;
+                });
+
+                return {...student, avgScore};
+            });
+
+            let subjectCounts = {};
+            studentData.forEach(student => {
+                Object.entries(student.subject).forEach(([subject, score]) => {
+                    if(!subjectCounts[subject]) subjectCounts[subject] = {count:0, below40:0};
+                    subjectCounts[subject].count++;
+                    if(score < 40) subjectCounts[subject].below40++;
+                });
+            });
+
+            Object.entries(subjectCounts).forEach(([subject, {count, below40}]) => {
+                if(below40 / count > 0.5) insights.difficultSub[subject] =` ${((below40 / count) * 100).toFixed(2)}% `;
+            });
+
+            return insights;
         }
-        if(score.some(([,score])=>score<35)){
-            insights.failedStudent.push(student.name)
-        }
-        score.forEach(([subject])=>{
-            insights.frequencysub[subject]=(insights.frequencysub[subject]||0)+1
-        })
-        return {...student,avg}
-      })
-      let subCount={}
-      studentData.forEach(student=>{
-        Object.entries(student.subject).forEach(([subject,score])=>{
-            if(!subCount[subject]){
-                subCount[subject]={count:0,lessThan40:0}
-            }
-            subCount[subject].count++
-            if(score<40){
-                subCount[subject].lessThan40++
-            }
-        })
-      })
-      Object.entries(subCount).forEach(([subject,{count,lessThan40}])=>{
-        if(lessThan40/count>0.5){
-            insights.difficultSub[subject]=`${((lessThan40/count)*100).toFixed(2)}%`
-        }
-      })
-      return insights
-    },
-  };
+    };
+
 }
 
 let performer=createStudentManager()
@@ -94,4 +91,4 @@ performer.updateScore("Miki","Math",90)
 performer.updateScore("Deba","Eng",33)
 console.log(performer.getStudentDetails("Miki"))
 console.log(performer.getSortedStudents("averageScore"));
-console.log(performer.generateInsight());
+console.log(performer.generateInsights());
